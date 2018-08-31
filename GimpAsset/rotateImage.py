dc7e3b063f23f45c67addde28b4f90fa2d1046b3
willlmac321 <willma321@gmail.com>
2018-08-30 19:00:51 -0500

    added pixel check for colors and got the image broken up

--- GimpAsset/rotateImage.py
+++ GimpAsset/rotateImage.py
@@ -1,5 +1,6 @@
 import math
-from PIL import Image
+from PIL import Image, ImageStat
+from functools import reduce
 
 def rotateAndSaveImages():
     ##Grab the images, rotate them and save to a folder
@@ -14,34 +15,45 @@ def rotateAndSaveImages():
 
 def smallifyToMap():
     ##Combine the save images into one spritemap and create a font file
-    for name in ("Body", "Arm"):
-        row = 0
-        col = 0
-        blank_image = Image.new("RGB", (1440,2400))
-        blank_text = "info face=outBody size=50 bold=0 italic=0 charset=ascii unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0 outline=0"
-        blank_text += "\ncommon lineHeight=240 base=240 scaleW=256 scaleH=256 pages=1 packed=0"
-        blank_text +=    "\npage id=0 file=\"out" + name + ".png\""
-        blank_text +=    "\nchars count=60"
-        for ang in range(0, 5, 6):
-            im3 = Image.open(name + "Rotate/dino" + name + str(ang) + ".png")
-            height = 24
-            width = 24
-            imgwidth, imgheight = im3.size
-            for i in range(0,imgheight,height):
-                for j in range(0,imgwidth,width):
-                    #box = (240, 240, 240+j,240+i)
-                    box = (j, i, j+width, i+height)
-                    print(box)
-                    a = im3.crop(box)
+    #for name in ("Body", "Arm"):
+    name = "Body"
+    row = 0
+    col = 0
+    width = 24
+    height = 24
+    pIX, pIY = 1440,2400
+    pX, pY = 0,0
+    blank_image = Image.new("RGB", (pIX, pIY))
+    blank_text = "info face=outBody size=50 bold=0 italic=0 charset=ascii unicode=0 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0 outline=0"
+    blank_text += "\ncommon lineHeight=240 base=240 scaleW=256 scaleH=256 pages=1 packed=0"
+    blank_text += "\npage id=0 file=\"out" + name + ".png\""
+    blank_text += "\nchars count=60"
+    ##do this for each image representing a minute/every 6 degrees
+    for ang in range(0, 5, 6):
+        im3 = Image.open(name + "Rotate/dino" + name + str(ang) + ".png")
+        im3
+        imgwidth, imgheight = im3.size
+        for i in range(0,imgwidth, width):
+            for j in range(0,imgheight, height):
+                #box = (240, 240, 240+j,240+i)
+                box = (j, i, j+height, i+width,)
+                a = im3.crop(box)
+                t = ImageStat.Stat(a).sum
+                if any(l > 0 for l in t):
+                    print(t)
                     try:
-                        o = a.crop(box)
                         if ang/6 % 6 == 0 and ang != 0:
                             row += 240
                             col = 0
                         #print(col, row)
-                        blank_image.paste(o, (i,j))
+                        if pX >= pIX:
+                            pX = 0
+                            pY += height
+                        blank_image.paste(a, (pX, pY))
+                        pX += width
+                        blank_image
                         #print('x: ' + str(j),'y: '  + str(i))
-                        blank_text += "\nchar id=" + str(int(ang/6+i/24 + j/24 +33)) + " x=" + str(row+i/24) + " y=" + str(col+i/24) + " width=240 height=240 xoffset=0 yoffset=0 xadvance=240 page=0 chnl=15"
+                        blank_text += "\nchar id=" + str(int(ang/6 + i/24 + j/24 + 33)) + " x=" + str(row*i/24) + " y=" + str(col*j/24) + " width=240 height=240 xoffset=0 yoffset=0 xadvance=240 page=0 chnl=15"
                         col +=240
                         blank_image.save("out" + name + ".png")
                         with open("fontOut"+ name + ".fnt", "w") as text_file:
@@ -51,6 +63,7 @@ def smallifyToMap():
 
 def main():
     smallifyToMap()
+    print("done")
 
 if __name__ == "__main__":
     main()
